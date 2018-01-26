4131f5b421e64ce8ae57fb172b34e0cfc60b1ff6
sumit kumar <sumit_jaiswal18@live.com>
2018-01-25 19:35:10 -0500

    Added some vars and a function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// alerts are annoying, let's use console.log
+
+  console.log('Welcome');
+  /*
+  This will now log to your console
+  */
+
+  //Create a function that updates a DOM element
+  function change() {
+    //function body. where it all goes down.
+    document.getElementById('foo').innerHTML='Welcome';
+  }
+
+  var name='Sumit'; //store a string in a Javascript
+  var numberofWidgets=10; //numberofWidgets
+  var iscodingcool=true; //boolean
